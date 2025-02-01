35c09a6a35f661f2c7a85714a27c8632da9bfa9d
Nicholas Serencovich Carvalho <153335120+nicholas-sc-08@users.noreply.github.com>
2025-02-01 17:47:45 -0300

    Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Usuario{
+
+    String nome;
+    String cpf;
+    String email;
+    String senha;
+
+    public Usuario(String nome, String cpf, String email, String senha){
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    };
+}
